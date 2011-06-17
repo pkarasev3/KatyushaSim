4a16b996e28cd64ebd25bd0c865b12683a3a19f1
pkarasev3 <karasevpa@gmail.com>
2011-06-16 23:51:39 -0400

    reshuffle a bit, remove redundant tests funcs

---
+++ render_camera/drawRocket_base.m
@@ -0,0 +1,9 @@
+function rocket = drawRocket_base( rocket )
+% make the core part of rocket drawing.
+% each sub-type of rocket drawing sets some trickier aspects of parameters
+% that are not for "user editing" externally, e.g. stuff relating to 
+% plume profile and histogram shaping. user just wants to set position
+% and base # of points
+  
+
+end
